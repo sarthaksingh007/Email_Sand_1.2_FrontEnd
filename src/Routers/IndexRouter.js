3817197c080567b653317b9a2f746d5f65ba8b48
sarthak Singh <100351826+sarthaksingh007@users.noreply.github.com>
2023-12-11 19:33:43 +0530

    fix navbar isssue

--- src/Routers/IndexRouter.js
+++ src/Routers/IndexRouter.js
@@ -5,8 +5,7 @@ import {
   Route,
 } from "react-router-dom";
 import AppRouter from "./AppRouter";
-import Home from "../components/common/SendEmailPagesUI/Componets/Nav.js"
-// import Home from "../Pages/UserEnd/Home";
+import Home from "../components/common/SendEmailPagesUI/Componets/Navbar/Navbar.js"
 import NotFound from "../Pages/NotFound";
 
 const IndexRouter = () => {
