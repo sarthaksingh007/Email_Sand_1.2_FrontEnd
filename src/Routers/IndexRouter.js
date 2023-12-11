170864e134dfca53962bc0c4bf02ebd714d95db8
sarthak Singh <100351826+sarthaksingh007@users.noreply.github.com>
2023-12-11 22:00:26 +0530

    remove path

--- src/Routers/IndexRouter.js
+++ src/Routers/IndexRouter.js
@@ -6,7 +6,6 @@ import {
 } from "react-router-dom";
 import AppRouter from "./AppRouter";
 import Home from "../components/common/SendEmailPagesUI/Componets/Navbar/Navbar.js"
-import NotFound from "../Pages/NotFound";
 
 const IndexRouter = () => {
   return (
